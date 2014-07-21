e22a7012ea8af296c5810475e3552ada6a80d8ca
Sanjeev & Mark R <student@epicodus.com>
2014-07-21 10:53:50 -0700

    Refactor

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,14 +1,15 @@
 def fizz_buzz(num)
   varcount = []
   (1..num).to_a.each {|i|
-    if i.modulo(3) == 0 && i.modulo(5) == 0
-      varcount.push("fizzbuzz")
-    elsif i.modulo(3) == 0
-      varcount.push("fizz")
-    elsif i.modulo(5) == 0
-      varcount.push("buzz")
+    unless i.modulo(3) == 0 && i.modulo(5) == 0
+     (i.modulo(3) == 0) ?
+        varcount.push("fizz")
+      :  (i.modulo(5) == 0) ?
+        varcount.push("buzz")
+      :
+        varcount.push(i)
     else
-      varcount.push(i)
+      varcount.push("fizzbuzz")
     end
   }
   varcount
