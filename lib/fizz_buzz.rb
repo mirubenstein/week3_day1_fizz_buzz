7726be4e0c6f4924c70b12883e8ddec19f1b372f
Sanjeev & Mark R <student@epicodus.com>
2014-07-21 10:42:37 -0700

    Added 'buzz' and 'fizzbuzz' functions'

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,12 +1,16 @@
 def fizz_buzz(num)
   varcount = []
   (1..num).to_a.each {|i|
-    if i.modulo(3) == 0
+    if i.modulo(3) == 0 && i.modulo(5) == 0
+      varcount.push("fizzbuzz")
+    elsif i.modulo(3) == 0
       varcount.push("fizz")
+    elsif i.modulo(5) == 0
+      varcount.push("buzz")
     else
       varcount.push(i)
     end
   }
   varcount
 end
-puts fizz_buzz(5)
+puts fizz_buzz(15)
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -8,4 +8,10 @@ describe('fizz_buzz') do
   it("for every number divisible by 3, return fizz") do
     fizz_buzz(3).should(eq([1, 2,'fizz']))
   end
+  it("for every number divisible by 5, return buzz") do
+    fizz_buzz(5).should(eq([1, 2, 'fizz', 4, 'buzz']))
+  end
+  it("for every number divisible by 15, return fizzbuzz") do
+    fizz_buzz(15).should(eq([1, 2, 'fizz', 4, 'buzz', 'fizz', 7, 8 , 'fizz', 'buzz', 11, 'fizz', 13, 14, 'fizzbuzz']))
+  end
 end
